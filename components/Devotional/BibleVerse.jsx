9cbc9b0de9627eef1b8e8eb144198056d562c9f1
clement <clementadebola54@gmail.com>
2025-04-10 13:27:47 +0100

    new

---
+++ components/Devotional/BibleVerse.jsx
@@ -0,0 +1,29 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function BibleVerse({ verse, passage }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.verse}>{verse}</Text>
+      <Text style={styles.passage}>{passage}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  verse: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+  passage: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: '600',
+    textAlign: 'center',
+    marginTop: 10,
+  }
+});
